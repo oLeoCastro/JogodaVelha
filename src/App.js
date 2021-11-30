bab07eb1f9a9ffbea4273e1eea95b2f88f551501
Leo Castro <leocastro98@outlook.com>
2021-11-30 19:41:20 -0300

    add jogo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Square from './components/Square'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full flex items-center justify-center">
+      <div className="border rounded-xl mt-20">
+        <h1 className="text-xxl font-bold p-2">Jogo da Velha</h1>
+        <div className="border rounded w-56 grid grid-cols-3">
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+        </div>
+      </div>
     </div>
   );
 }
